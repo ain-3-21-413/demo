7138de6597f1eef6f3c1a6944dbe2ac02a9cdca5
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-02-22 23:33:54 +0600

    commit

---
+++ src/main/java/com/example/demo/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.example.demo.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
+
+
+}
